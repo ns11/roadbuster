60fa11cf05f1ed8fddfb10d2dfd131a62d5d6a31
Aiky30 <aiky30@yahoo.com>
2018-10-31 10:50:29 +0000

    Refactored the management command as per PR feedback to prevent the management command executing twice.

--- roadbuster/management/commands/reload_database.py
+++ roadbuster/management/commands/reload_database.py
@@ -1,17 +1,21 @@
-from django.core.management.base import BaseCommand, CommandError
+import os
 from django.core.management import call_command
+from django.core.management.commands import runserver
 
 
-class Command(BaseCommand):
-    
+class Command(runserver.Command):
+
     def handle(self, *args, **options):
-        
-        self.stdout.write("Reloading database")
+        """
+        Reload the database by emptying, re populating and then running the server
+        """
+        # Catch an issue where runserver spawns multiple threads and runs this command twice.
+        if not os.environ.get('RUN_MAIN', False):
+            self.stdout.write("Reloading database")
+            call_command('flush', '--noinput')
+            call_command('clear_index', '--noinput')
+            call_command('migrate')
+            call_command('bootstrap')
+            call_command('runserver')
 
-        call_command('flush')
-        call_command('clear_index', '--noinput')
-        call_command('migrate')
-        call_command('bootstrap')
-        call_command('runserver')
-
-
+        super().handle(*args, **options)
